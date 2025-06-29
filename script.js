fcd5429735eb5abc72309cac0b53120718c1111b
Danushka-Senevirathne <danush15100@gmail.com>
2025-06-29 22:36:19 +0530

    29/06/2025
    accessories.js file added

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // import laptop data form object array
 import {laptops} from './js/laptop.js';
+import {accessories} from './js/accessories.js';
 
-
+// for laptop
 const laptop_container = document.getElementById('laptop_container');
 
 laptops.forEach(data => {    
@@ -16,4 +17,21 @@ laptops.forEach(data => {
         <button>View Specs</button>
     `;
     laptop_container.appendChild(laptop_card);
+});
+
+// for accessories
+const accessories_container = document.getElementById('accessories_container');
+
+accessories.forEach(data => {    
+    const accessories_card = document.createElement('div');
+    accessories_card.className = "laptop_card";
+    accessories_card.innerHTML = `
+        <div class="lap_image">
+            <img src="${data.main_image}" alt="${data.name}">
+        </div>
+        <h5>${data.brand} ${data.name}</h5>
+        <p>${data.price}</p>
+        <button>View Specs</button>
+    `;
+    accessories_container.appendChild(accessories_card);
 });
