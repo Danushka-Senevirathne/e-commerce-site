98861ffd867ef219592574425686fe2c04479ab8
Danushka-Senevirathne <danush15100@gmail.com>
2025-07-02 22:42:04 +0530

    02/07/2025
    json file updated

--- js/laptop.js
+++ js/laptop.js
@@ -23,4 +23,28 @@ export const laptops = [
         specs:"13.6 Inch M2 Chip 16GB RAM 256GB SSD",
         countity:10
     },
+	   {
+        id:3,
+        main_image:"https://buyabans.com/cdn-cgi/imagedelivery/OgVIyabXh1YHxwM0lBwqgA/product/16959/EVy4OzXfAlXAQEt38PU3UO4y3dbD3wwFtf37f6R3.webp/public",
+        secondary_image_1:"https://buyabans.com/cdn-cgi/imagedelivery/OgVIyabXh1YHxwM0lBwqgA/product/16959/8KYEt6wH5ZYXD3N2pjsVNAcKUKYEUl5vjrt6JO2c.webp/public",
+        secondary_image_2:"https://buyabans.com/cdn-cgi/imagedelivery/OgVIyabXh1YHxwM0lBwqgA/product/16958/YLdlBVCpWwaaQ7EYdGRRx1DZIjaT6rku3jp7jxsE.webp/public",
+        secondary_image_3:"https://buyabans.com/cdn-cgi/imagedelivery/OgVIyabXh1YHxwM0lBwqgA/product/16958/n39Wj9OTY71wuLfqcCbgrCGqtHw2933iOeiGoIpc.webp/public",
+        brand:"Apple",
+        name:"Mackbook Air 2025",
+        price: 425000,
+        specs:"13.6 Inch M2 Chip 16GB RAM 256GB SSD",
+        countity:10
+    },
+    {
+        id:4,
+        main_image:"https://buyabans.com/cdn-cgi/imagedelivery/OgVIyabXh1YHxwM0lBwqgA/product/8575/apppclmqkw3paa_jekaka.jpg/public",
+        secondary_image_1:"https://buyabans.com/cdn-cgi/imagedelivery/OgVIyabXh1YHxwM0lBwqgA/product/16959/8KYEt6wH5ZYXD3N2pjsVNAcKUKYEUl5vjrt6JO2c.webp/public",
+        secondary_image_2:"https://buyabans.com/cdn-cgi/imagedelivery/OgVIyabXh1YHxwM0lBwqgA/product/16958/YLdlBVCpWwaaQ7EYdGRRx1DZIjaT6rku3jp7jxsE.webp/public",
+        secondary_image_3:"https://buyabans.com/cdn-cgi/imagedelivery/OgVIyabXh1YHxwM0lBwqgA/product/16958/n39Wj9OTY71wuLfqcCbgrCGqtHw2933iOeiGoIpc.webp/public",
+        brand:"Apple",
+        name:"Mackbook 2024",
+        price: 225000,
+        specs:"13.6 Inch M2 Chip 16GB RAM 256GB SSD",
+        countity:10
+    },
 ]
